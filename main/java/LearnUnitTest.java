e1132cc7b5f9eb52489c46955ca1a98cf9cd234c
Кудрявцев Алексей Сергеевич <alser@skbkontur.ru>
2017-08-01 11:40:13 +0500

    Создали проект.

---
+++ main/java/LearnUnitTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by alser on 06.07.2017.
+ */
+public class LearnUnitTest {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 2;
+        int sum;
+        sum = x + y;
+        System.out.println(sum);
+    }
+}
---
+++ test/java/LearnUnitTestTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+        import static org.junit.Assert.assertEquals;
+
+public class LearnUnitTestTest extends LearnUnitTest {
+    @Test
+    public void main() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
